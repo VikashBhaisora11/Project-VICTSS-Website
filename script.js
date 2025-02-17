aa3330dfcd4ede09152c67e596d6a04a36341a2a
VikashBhaisora11 <vikashbhaisora1@gmail.com>
2025-02-18 02:03:11 +0530

    whole main page done with some minor changes

--- script.js
+++ script.js
@@ -5,3 +5,43 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open')
 }
+// slide 1
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop:true,
+    slidesPerView: "auto",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+//   slide2
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
